b7c2ac0739a62c845ab4f9aefe28cf9ce37f83c8
Emmanuel Zenderock <contact@zenderock.me>
2025-10-16 18:06:08 +0100

    update

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -128,7 +128,7 @@ export default function HeroHome() {
             <div className="relative aspect-video rounded-2xl bg-gray-900 px-5 py-3 shadow-xl before:pointer-events-none before:absolute before:-inset-5 before:border-y before:[border-image:linear-gradient(to_right,transparent,--theme(--color-slate-300/.8),transparent)1] after:absolute after:-inset-5 after:-z-10 after:border-x after:[border-image:linear-gradient(to_bottom,transparent,--theme(--color-slate-300/.8),transparent)1]">
               <div className="relative mb-8 flex items-center justify-between before:block before:h-[9px] before:w-[41px] before:bg-[length:16px_9px] before:[background-image:radial-gradient(circle_at_4.5px_4.5px,var(--color-gray-600)_4.5px,transparent_0)] after:w-[41px]">
                 <span className="text-[13px] font-medium text-white">
-                  servelink.com
+                  servel.ink
                 </span>
               </div>
               <div className="font-mono text-gray-500 [&_span]:opacity-0">
